0d5b322931770eb0cdfc7b7f8803354b165a9661
Kaivan Shah <48644270+kaivanshah14@users.noreply.github.com>
2021-11-11 00:39:01 +0530

    Create TwoSum.cpp

---
+++ TwoSum.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    map<int,int> m;
+    vector<int> v;
+    int n= nums.size();
+    for(int i=0;i<n;i++)
+    {
+
+        int diff = target - nums[i];
+        if(m.find(diff) != m.end())
+        {
+            auto p = m.find(diff);
+            v.push_back(p->second);
+            v.push_back(i);
+        }
+            m.insert(make_pair(nums[i],i));
+    }
+
+    return v;
+}
+
+
+int main(){
+    vector<int> v;
+    v={1,6,3,2,5};
+    vector<int> result= twoSum (v, 11);
+    for(int i: result)
+    {
+        cout<<i<<endl;
+    }
+
+return 0;
+
+}
